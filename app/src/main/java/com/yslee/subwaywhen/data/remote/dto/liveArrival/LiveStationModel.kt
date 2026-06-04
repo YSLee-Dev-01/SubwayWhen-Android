ba0cb054fdf78912b0abf233c45cd46a9fab0300
YSLee-Dev-01 <ysleedev01@gmail.com>
2026-06-04 21:22:26 +0900

    Fix(Data > DTO): RealtimeStationArrival.useState computed property 추가 (CC)

--- app/src/main/java/com/yslee/subwaywhen/data/remote/dto/liveArrival/LiveStationModel.kt
+++ app/src/main/java/com/yslee/subwaywhen/data/remote/dto/liveArrival/LiveStationModel.kt
@@ -26,4 +26,20 @@ data class RealtimeStationArrival(
     // TotalLoadModelImpl.liveArrivalSplit()에서 statnFid 조회 후 채워지는 파생 필드
     // iOS TotalLoadModel.nextAndBackStationSearch() 대응
     @kotlinx.serialization.Transient val backStationName: String = "",
-)
+) {
+    /**
+     * iOS RealtimeStationArrival.useState 대응.
+     * code 기반으로 현재 상태 메시지를 반환한다.
+     */
+    val useState: String
+        get() = when (code) {
+            "0"  -> "$stationName 진입"
+            "1"  -> "$stationName 도착"
+            "2"  -> "$stationName 출발"
+            "3"  -> "전역 출발"
+            "4"  -> "전역 진입"
+            "5"  -> "전역 도착"
+            "99" -> previousStation?.let { "$it 부근" } ?: subPrevious
+            else -> code
+        }
+}
